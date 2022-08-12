68eab448f8dbc0d316489361050737deb4c02a4a
k-mitani <k-mitani@unique21.co.jp>
2022-08-12 13:44:19 +0900

    Martial 敵の動きの実装まで

--- Assets/Martial/MartialCharacter.cs
+++ Assets/Martial/MartialCharacter.cs
@@ -1,56 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MartialCharacter : MonoBehaviour
 {
+    public bool isPlayer;
+    public bool isEnemy => !isPlayer;
+    public bool IsOpponent(MartialCharacter target) => isPlayer != target.isPlayer;
+
     public int prowess;
-    public GameObject moveRange;
-    public GameObject attackRange;
-    public Transform shadowChild;
+    public int hp = 100;
+
     public Transform shadow;
+    public Transform moveRange;
+    public Transform attackRange;
+    public SphereCollider hitRange;
+    public MeshRenderer selectionBox;
+    public Canvas canvas;
+    public Slider sliderHp;
 
     public Rigidbody rb;
 
     public float MaxMoveAmount => prowess / 25f;
+
+    public Vector3? MovePhaseDestination { get; set; }
+    public Quaternion? MovePhaseFinalDirection { get; set; }
+    public bool IsAlive => hp > 0;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
 
+        // 必要な要素を取得する。
+        var parent = transform.parent;
+        shadow = parent.Find("Common/Shadow");
+        moveRange = parent.Find("Common/MoveRange");
+        attackRange = parent.Find("Common/AttackRange");
+        hitRange = parent.Find("Common/HitRange").GetComponent<SphereCollider>();
+        selectionBox = parent.Find("Common/SelectionBox").GetComponent<MeshRenderer>();
+        canvas = parent.Find("Common/Canvas").GetComponent<Canvas>();
+        canvas.transform.SetParent(transform);
+        sliderHp = canvas.transform.Find("Hp").GetComponent<Slider>();
+        if (isEnemy) sliderHp.fillRect.GetComponent<Image>().color = Color.red;
+
+        // 移動範囲をセットする。
         var scale = moveRange.transform.localScale;
         scale *= MaxMoveAmount;
         scale.y = moveRange.transform.localScale.y;
         moveRange.transform.localScale = scale;
 
-        moveRange.SetActive(false);
-        attackRange.SetActive(false);
-        shadow.gameObject.SetActive(false);
+        HideMoveRange();
+        HideAttackRange();
+        HideShadow();
+        HideSelectionBox();
     }
 
-    public void ShowMoveRange()
+    public void ShowMoveRange(Vector3? position = null)
     {
-        moveRange.transform.position = transform.position;
-        moveRange.SetActive(true);
+        if (position != null) moveRange.position = position.Value;
+        moveRange.GetComponent<Renderer>().enabled = true;
     }
     public void HideMoveRange()
     {
-        moveRange.SetActive(false);
+        moveRange.GetComponent<Renderer>().enabled = false;
     }
 
-    public void ShowAttackRange()
+    public void ShowAttackRange(Vector3? position = null)
     {
-        attackRange.SetActive(true);
+        if (position != null) attackRange.position = position.Value;
+        attackRange.GetComponent<Renderer>().enabled = true;
     }
     public void HideAttackRange()
     {
-        attackRange.SetActive(false);
+        attackRange.GetComponent<Renderer>().enabled = false;
     }
 
     public void ShowShadow()
     {
-        CopyTransforms(shadowChild.transform, transform);
+        CopyTransforms(transform, shadow);
         shadow.gameObject.SetActive(true);
     }
     public void HideShadow()
@@ -58,21 +88,174 @@ public class MartialCharacter : MonoBehaviour
         shadow.gameObject.SetActive(false);
     }
 
-    private void CopyTransforms(Transform a, Transform b)
+    public void ShowSelectionBox()
     {
-        for (int i = 0; i < a.childCount; i++)
+        selectionBox.gameObject.SetActive(true);
+    }
+    public void HideSelectionBox()
+    {
+        selectionBox.gameObject.SetActive(false);
+    }
+
+    private void CopyTransforms(Transform src, Transform dest)
+    {
+        for (int i = 0; i < dest.childCount; i++)
         {
-            var childA = a.GetChild(i);
-            var childB = b.GetChild(i);
-            childA.localPosition = childB.localPosition;
-            childA.localScale = childB.localScale;
-            childA.localRotation = childB.localRotation;
-            CopyTransforms(childA, childB);
+            var childDest = dest.GetChild(i);
+            var childSrc = src.GetChild(i);
+            childDest.localPosition = childSrc.localPosition;
+            childDest.localScale = childSrc.localScale;
+            childDest.localRotation = childSrc.localRotation;
+            CopyTransforms(childSrc, childDest);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    public void OnBeforeMove()
+    {
+        if (!IsAlive) return;
+
+        // 移動する必要があるなら
+        if (MovePhaseDestination != null)
+        {
+            GetComponent<Animator>().SetBool("IsMoving", true);
+
+            // 移動先を向く。
+            transform.LookAt(MovePhaseDestination.Value);
+
+            // 速度をセットする。
+            var end = new Vector3(MovePhaseDestination.Value.x, 0, MovePhaseDestination.Value.z);
+            var start = new Vector3(transform.position.x, 0, transform.position.z);
+            var distance = (end - start).magnitude;
+            rb.velocity = transform.forward * distance;
+        }
+
+    }
+
+    public void OnAfterMove()
+    {
+        if (!IsAlive) return;
+
+        if (MovePhaseDestination != null)
+        {
+            GetComponent<Animator>().SetBool("IsMoving", false);
+
+            // 移動を止める。
+            rb.velocity = Vector3.zero;
+            
+            MovePhaseDestination = null;
+        }
+
+        // 設定した方向に向く。
+        if (MovePhaseFinalDirection != null)
+        {
+            transform.rotation = MovePhaseFinalDirection.Value;
+
+            MovePhaseFinalDirection = null;
+        }
+
+        // 攻撃範囲をセットする。
+        moveRange.position = transform.position;
+        attackRange.SetPositionAndRotation(transform.position, transform.rotation);
+        hitRange.transform.position = transform.position;
+        selectionBox.transform.position = transform.position;
+    }
+
+    private MartialCharacter attackTarget;
+    private bool isAnimating;
+    internal IEnumerator OnAttack(IEnumerable<MartialCharacter> all)
+    {
+        if (!IsAlive) yield break;
+
+        attackTarget = null;
+        var enemiesInAttackRange = new List<MartialCharacter>();
+        foreach (var enemy in all.Except(new[] { this }).Where(x => IsOpponent(x)))
+        {
+            if (!enemy.IsAlive) continue;
+
+            if (enemy.hitRange.bounds.Intersects(attackRange.GetComponent<MeshCollider>().bounds))
+            {
+                enemiesInAttackRange.Add(enemy);
+            }
+        }
+        // 攻撃範囲内に敵がいる場合。
+        if (enemiesInAttackRange.Count > 0)
+        {
+            ShowAttackRange();
+
+            // プレーヤーの場合
+            if (isPlayer)
+            {
+                // 対象が1つしかないならそれを選択する。
+                if (enemiesInAttackRange.Count == 1)
+                {
+                    attackTarget = enemiesInAttackRange[0];
+                }
+                // 対象が複数あればプレーヤーに選択させる。
+                else
+                {
+                    var gm = GameObject.Find("GameManager").GetComponent<MartialGameManager>();
+                    yield return gm.SelectCharacter(enemiesInAttackRange);
+                    attackTarget = gm.SelectCharacterResult;
+                }
+            }
+            // NPCの場合
+            else
+            {
+                // ランダムに攻撃対象を選ぶ
+                attackTarget = enemiesInAttackRange[Random.Range(0, enemiesInAttackRange.Count)];
+            }
+            isAnimating = true;
+            GetComponent<Animator>().SetTrigger("Attack1");
+            while (isAnimating) yield return new WaitForSeconds(0.1f);
+            HideAttackRange();
+        }
+    }
+
+    public void OnDamage(MartialCharacter opponent)
+    {
+        var damage = 100f
+            * opponent.prowess
+            / prowess
+            ;
+        GetComponent<Animator>().SetTrigger("Damage1");
+
+        StartCoroutine(damaging());
+        IEnumerator damaging()
+        {
+            hp = Mathf.Max(0, hp - (int)damage);
+            sliderHp.value = hp;
+            if (hp == 0)
+            {
+                GetComponent<Animator>().SetTrigger("Death4");
+                GetComponent<CapsuleCollider>().enabled = false;
+
+                isAnimating = true;
+                while (isAnimating) yield return new WaitForSeconds(0.1f);
+                //GetComponent<Renderer>().enabled = false;
+            }
+        }
+    }
+
+
+    public void OnAnimationEvent(string name)
+    {
+        if (name.Equals("Impact"))
+        {
+            attackTarget.OnDamage(this);
+        }
+        else if (name.Equals("End"))
+        {
+            isAnimating = false;
+        }
+        else if (name.Equals("DeathEnd"))
+        {
+            isAnimating = false;
+        }
     }
 }
--- Assets/Martial/MartialGameManager.cs
+++ Assets/Martial/MartialGameManager.cs
@@ -1,115 +1,303 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class MartialGameManager : MonoBehaviour
 {
-    private GameState state = GameState.SelectAction;
-    private GameObject selectActionUi;
+    private GameStates states;
+    private GameObject uiSelectAction;
+    private Transform pointer;
 
     public MartialCharacter player;
+    public List<MartialCharacter> enemies = new();
+    public IEnumerable<MartialCharacter> characters => new[] { player }
+        .Concat(enemies)
+        .OrderByDescending(c => c.prowess);
 
-    private Transform pointer;
+    public Material defaultSelectBoxMaterial;
+    public Material selectedSelectBoxMaterial;
+
     // Start is called before the first frame update
     void Start()
     {
-        selectActionUi = GameObject.Find("SelectActionUI");
+        states = new GameStates(this);
+        
+        uiSelectAction = GameObject.Find("SelectActionUI");
         pointer = GameObject.Find("Pointer").GetComponent<Transform>();
+        
+        player.GetComponent<Animator>().SetBool("IsLongSword", true);
+        states.Activate(s => s.SelectAction);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // マウスポインタの位置を取得する。
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (!player.GetComponent<Animator>().GetBool("IsMoving"))
+        // キャラ選択中の場合
+        if (isSelectingCharacter)
         {
-            if (Physics.Raycast(ray, out var hit, 100f, 1 << LayerMask.NameToLayer("MartialFloor")))
+            var prevHover = selectCharacterMouseHoveredBox;
+            var shouldClearSelection = prevHover != null;
+            if (Physics.Raycast(ray, out var hit, 100f, 1 << LayerMask.NameToLayer("MartialSelectionBox")))
             {
-                pointer.position = hit.point;
+                if (hit.transform == selectCharacterMouseHoveredBox)
+                {
+                    prevHover = null;
+                    shouldClearSelection = false;
+                }
+                else
+                {
+                    selectCharacterMouseHoveredBox = hit.transform;
+                    selectCharacterMouseHoveredBox.GetComponent<MeshRenderer>().material = selectedSelectBoxMaterial;
+                    shouldClearSelection = false;
+                }
+            }
+            if (prevHover != null)
+            {
+                prevHover.GetComponent<MeshRenderer>().material = defaultSelectBoxMaterial;
+            }
+            if (shouldClearSelection)
+            {
+                selectCharacterMouseHoveredBox = null;
+            }
+        }
+        else if (Physics.Raycast(ray, out var hit, 100f, 1 << LayerMask.NameToLayer("MartialFloor")))
+        {
+            pointer.position = hit.point;
 
-                if (state == GameState.SetMovePosition)
-                {
-                    player.transform.LookAt(pointer.position);
-                    player.shadow.rotation = player.transform.rotation;
-                    player.shadow.position = player.transform.position;
-                    var distance = Mathf.Min(Mathf.Sqrt(player.MaxMoveAmount), (pointer.position - player.transform.position).magnitude);
-                    player.shadow.position += player.transform.forward * distance;
-                }
-                else if (state == GameState.SetRotation)
-                {
-                    player.shadow.LookAt(pointer.position);
-                }
+            if (states.SetMovePosition.IsActive)
+            {
+                // 移動先に影を配置する。
+                player.transform.LookAt(pointer.position);
+                player.shadow.rotation = player.transform.rotation;
+                player.shadow.position = player.transform.position;
+                var distance = Mathf.Min(Mathf.Sqrt(player.MaxMoveAmount), (pointer.position - player.transform.position).magnitude);
+                player.shadow.position += player.transform.forward * distance;
+            }
+            else if (states.SetRotation.IsActive)
+            {
+                // ポインターの方を向く。
+                player.shadow.LookAt(pointer.position);
+                player.attackRange.rotation = player.shadow.rotation;
             }
         }
 
+        // 左クリック: 進む
         if (Input.GetMouseButtonDown(0))
         {
-            if (state == GameState.SetMovePosition)
+            if (states.SetMovePosition.IsActive)
             {
-                state = GameState.SetRotation;
-                player.HideMoveRange();
-                player.ShowAttackRange();
+                // 現在の影の位置を移動先する。
+                player.MovePhaseDestination = player.shadow.position;
+                states.Activate(s => s.SetRotation);
             }
-            else if (state == GameState.SetRotation)
+            else if (states.SetRotation.IsActive)
             {
-                state = GameState.Move;
-                player.HideShadow();
-                player.HideAttackRange();
-                player.GetComponent<Animator>().SetBool("IsMoving", true);
-
-                var rotation = player.shadow.rotation;
-                var end = new Vector3(player.shadow.position.x, 0, player.shadow.position.z);
-                var start = new Vector3(player.transform.position.x, 0, player.transform.position.z);
-                var distance = (end - start).magnitude;
-                player.rb.velocity = player.transform.forward * distance;
-                StartCoroutine(aaa());
-                IEnumerator aaa()
+                player.MovePhaseFinalDirection = player.shadow.rotation;
+                states.Activate(s => s.Move);
+            }
+            else if (states.Move.IsActive)
+            {
+                if (isSelectingCharacter && selectCharacterMouseHoveredBox != null)
                 {
-                    yield return new WaitForSeconds(1);
-                    player.rb.velocity = Vector3.zero;
-                    player.transform.rotation = rotation;
-
-                    player.GetComponent<Animator>().SetBool("IsMoving", false);
-                    state = GameState.SelectAction;
-                    selectActionUi.SetActive(true);
+                    isSelectingCharacter = false;
                 }
             }
         }
+        // 右クリック: 戻る
         if (Input.GetMouseButtonDown(1))
         {
-            if (state == GameState.SetMovePosition)
+            if (states.SetMovePosition.IsActive)
             {
-                state = GameState.SelectAction;
-                selectActionUi.SetActive(true);
-                player.HideMoveRange();
+                states.Activate(s => s.SelectAction);
             }
-            if (state == GameState.SetRotation)
+            else if (states.SetRotation.IsActive)
             {
-                state = GameState.SetMovePosition;
-                player.HideAttackRange();
-                player.ShowMoveRange();
+                states.Activate(s => s.SetMovePosition);
             }
         }
     }
 
     public void OnMoveButtonClick()
     {
-        if (state == GameState.SelectAction)
+        if (states.SelectAction.IsActive)
         {
-            state = GameState.SetMovePosition;
-            selectActionUi.SetActive(false);
-            player.ShowMoveRange();
-            player.ShowShadow();
+            states.Activate(s => s.SetMovePosition);
         }
     }
 
-    private enum GameState
+    private void OnMoveStateEnter()
     {
-        SelectAction,
-        SetMovePosition,
-        SetRotation,
-        PreMove,
-        Move,
-        PostMove,
+        // 敵の行動を決める。
+        foreach (var enemy in enemies)
+        {
+            // とりあえずプレーヤーに向かってくるようにする。
+            var dest = player.transform.position;
+            var from = enemy.transform.position;
+            var distance = Mathf.Min(Mathf.Sqrt(enemy.MaxMoveAmount), (dest - from).magnitude);
+
+            enemy.MovePhaseDestination = from + (dest - from).normalized * distance;
+        }
+
+        // 各キャラの移動前の処理を行う。
+        foreach (var c in characters) c.OnBeforeMove();
+
+        StartCoroutine(Move());
+        IEnumerator Move()
+        {
+            // 移動する。
+            yield return new WaitForSeconds(1);
+
+            // 移動後の処理を行う。
+            foreach (var c in characters) c.OnAfterMove();
+            
+            // 当たり判定が効かないので1フレーム待つ。
+            yield return null;
+            yield return null;
+
+            // 攻撃処理を行う。
+            foreach (var c in characters) yield return c.OnAttack(characters);
+
+            states.Activate(s => s.SelectAction);
+        }
+    }
+
+    public MartialCharacter SelectCharacterResult { get; private set; }
+    private bool isSelectingCharacter;
+    private List<MartialCharacter> selectCharacterList;
+    private Transform selectCharacterMouseHoveredBox;
+    public IEnumerator SelectCharacter(List<MartialCharacter> targets)
+    {
+        SelectCharacterResult = null;
+        isSelectingCharacter = true;
+        selectCharacterList = targets;
+        selectCharacterMouseHoveredBox = null;
+
+        targets.ForEach(t => t.ShowSelectionBox());
+        while (isSelectingCharacter) yield return new WaitForSeconds(0.1f);
+        var selected = selectCharacterMouseHoveredBox.parent.parent.GetComponentInChildren<MartialCharacter>();
+        SelectCharacterResult = selected;
+        targets.ForEach(t => t.HideSelectionBox());
+    }
+
+    public class GameState
+    {
+        public MartialGameManager gm { get; set; }
+        public bool IsActive { get; set; }
+        
+        public virtual void OnEnter() { }
+        public virtual bool OnExitting() => true;
+        public virtual void OnExit() { }
+    }
+
+    public class GameStates
+    {
+        public GameState Current { get; private set; }
+
+        public SelectAction SelectAction { get; set; } = new SelectAction();
+        public SetMovePosition SetMovePosition { get; set; } = new SetMovePosition();
+        public SetRotation SetRotation { get; set; } = new SetRotation();
+        public PreMove PreMove { get; set; } = new PreMove();
+        public Move Move { get; set; } = new Move();
+        public PostMove PostMove { get; set; } = new PostMove();
+        public GameStates(MartialGameManager gm)
+        {
+            GetType()
+                .GetProperties()
+                .Select(p => p.GetValue(this))
+                .Where(o => o is GameState)
+                .ToList().ForEach(s => ((GameState)s).gm = gm);
+        }
+        public void Activate(System.Func<GameStates, GameState> selector)
+        {
+            var newState = selector(this);
+            var oldState = Current;
+            var shouldExit = oldState?.OnExitting() ?? true;
+            if (shouldExit)
+            {
+                if (oldState != null) oldState.IsActive = false;
+                newState.IsActive = true;
+                Current = newState;
+                oldState?.OnExit();
+                newState.OnEnter();
+            }
+        }
+    }
+
+    public class SelectAction : GameState
+    {
+        public override void OnEnter()
+        {
+            gm.uiSelectAction.SetActive(true);
+        }
+        public override void OnExit()
+        {
+            gm.uiSelectAction.SetActive(false);
+        }
+    }
+
+    public class SetMovePosition : GameState
+    {
+        public override void OnEnter()
+        {
+            gm.player.ShowShadow();
+            gm.player.ShowMoveRange();
+            gm.player.moveRange.position = gm.player.transform.position;
+        }
+        public override void OnExit()
+        {
+            gm.player.HideShadow();
+            gm.player.HideMoveRange();
+        }
+    }
+
+    public class SetRotation : GameState
+    {
+        public override void OnEnter()
+        {
+            gm.player.ShowShadow();
+            gm.player.ShowAttackRange();
+            gm.player.attackRange.position = gm.player.shadow.position;
+            gm.player.attackRange.rotation = gm.player.shadow.rotation;
+        }
+        public override void OnExit()
+        {
+            gm.player.HideShadow();
+            gm.player.HideAttackRange();
+        }
+    }
+
+    public class PreMove : GameState
+    {
+        public override void OnEnter()
+        {
+        }
+        public override void OnExit()
+        {
+        }
+    }
+
+    public class Move : GameState
+    {
+        public override void OnEnter()
+        {
+            gm.OnMoveStateEnter();
+        }
+        public override void OnExit()
+        {
+        }
+    }
+
+    public class PostMove : GameState
+    {
+        public override void OnEnter()
+        {
+        }
+        public override void OnExit()
+        {
+        }
     }
 }
+
