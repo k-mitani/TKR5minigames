9d961fdcf34c869ea6d217a61c4f020049b46170
k-mitani <k-mitani@unique21.co.jp>
2023-04-14 21:19:50 +0900

    tea ベース作成中

---
+++ Assets/Tea/TeaGameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeaGameManager : MonoBehaviour
+{
+    [SerializeField] private Texture2D[] icons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
