fc3cc4643fb84b71c19d6de0ef18dcf06429d323
k-mitani <k-mitani@unique21.co.jp>
2023-04-20 12:41:22 +0900

    wip

--- Assets/Tea/TeaGameManager.cs
+++ Assets/Tea/TeaGameManager.cs
@@ -1,20 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Timeline;
 
 public class TeaGameManager : MonoBehaviour
 {
+    [SerializeField] private float ShowReferenceDuration = 3;
+    [SerializeField] private TeaGameUI ui;
     [SerializeField] private Texture2D[] icons;
+
+    private const int MaxReferenceCount = 7;
+    private TeaItem[] itemsAll;
+    private TeaItem[] currentReferenceItems;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        itemsAll = icons.Select(x => new TeaItem()
+        {
+            Texture = x,
+        }).ToArray();
+
+        ui.DisableInput();
+        ui.SetSelectionItems(itemsAll);
+        ui.HideReferenceItems();
+        StartCoroutine(ArrangeWave());
+    }
+
+    private IEnumerator ArrangeWave()
+    {
+        // 入力不可能状態にする。
+        ui.DisableInput();
+
+        // 新しいランダムなリファレンスをセットする。
+        currentReferenceItems = Enumerable
+            .Range(0, MaxReferenceCount)
+            .Select(_ => Random.Range(0, itemsAll.Length))
+            .Select(x => itemsAll[x])
+            .ToArray();
+        ui.SetReferenceItems(currentReferenceItems);
         
+        // 一定時間表示する。
+        ui.ShowReferenceItems();
+        yield return new WaitForSeconds(ShowReferenceDuration);
+        ui.HideReferenceItems();
+
+        // 入力可能状態にする。
+        ui.EnableInput();
+    }
+
+    private void Ui_InputCompleted(object sender, System.EventArgs e)
+    {
+        throw new System.NotImplementedException();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ui.MoveSelectionRight();
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            ui.MoveSelectionLeft();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            ui.MoveSelectionDown();
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ui.InputSelectionItem();
+        }
+        else if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            ui.DeleteLastInput();
+        }
     }
+}
+
+public class TeaItem
+{
+    public Texture2D Texture { get; set; }
 }
--- Assets/Tea/TeaGameUI.cs
+++ Assets/Tea/TeaGameUI.cs
@@ -1,18 +1,143 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class TeaGameUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public event EventHandler InputCompleted;
+
+    private UIDocument uidoc;
+    
+    private VisualElement referencesContainer;
+    private List<VisualElement> references;
+
+    private VisualElement inputsContainer;
+    private List<VisualElement> inputs;
+    private List<TeaItem> inputsRaw = new List<TeaItem>();
+    private int nextInputIndex = 0;
+
+
+    private VisualElement selectionItemsContainer;
+    private List<VisualElement> selectionItems;
+    private TeaItem[] selectionItemsRaw;
+    private int selectionIndex;
+
+    private bool canInput = false;
+
+    void Awake()
     {
-        
+        TryGetComponent(out uidoc);
+        var root = uidoc.rootVisualElement;
+        referencesContainer = root.Q<VisualElement>("References");
+        references = referencesContainer.Children().ToList();
+
+        inputsContainer = root.Q<VisualElement>("Inputs");
+        inputs = inputsContainer.Children().ToList();
+        inputsRaw = inputs.Select(_ => (TeaItem)null).ToList();
+
+        selectionItemsContainer = root.Q<VisualElement>("SelectionItems");
+        selectionItems = selectionItemsContainer.Children().ToList();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MoveSelectionRight()
     {
+        if (!canInput) return;
+
+        var prevSelectionIndex = selectionIndex;
+        selectionIndex = (selectionIndex + 1) % selectionItems.Count;
+        selectionItems[prevSelectionIndex].RemoveFromClassList("selection-border--selected");
+        selectionItems[selectionIndex].AddToClassList("selection-border--selected");
+    }
+
+    public void MoveSelectionLeft()
+    {
+        if (!canInput) return;
         
+        var prevSelectionIndex = selectionIndex;
+        selectionIndex = (selectionItems.Count + selectionIndex - 1) % selectionItems.Count;
+        selectionItems[prevSelectionIndex].RemoveFromClassList("selection-border--selected");
+        selectionItems[selectionIndex].AddToClassList("selection-border--selected");
+    }
+
+    public void MoveSelectionDown()
+    {
+        if (!canInput) return;
+
+        var prevSelectionIndex = selectionIndex;
+        selectionIndex = (selectionItems.Count + selectionIndex - 1) % selectionItems.Count;
+        selectionItems[prevSelectionIndex].RemoveFromClassList("selection-border--selected");
+        selectionItems[selectionIndex].AddToClassList("selection-border--selected");
+    }
+
+    public void DeleteLastInput()
+    {
+        if (!canInput) return;
+        if (nextInputIndex == 0) return;
+
+        inputsRaw[nextInputIndex - 1] = null;
+        inputs[nextInputIndex - 1].style.backgroundImage = null;
+        nextInputIndex--;
+    }
+    
+    public void InputSelectionItem()
+    {
+        if (!canInput) return;
+        // すでに全部入力済みなら、最後の要素を置き換える。
+        if (nextInputIndex >= inputs.Count)
+        {
+            nextInputIndex = inputs.Count - 1;
+        }
+        
+        inputsRaw[nextInputIndex] = selectionItemsRaw[selectionIndex];
+        inputs[nextInputIndex].style.backgroundImage = inputsRaw[nextInputIndex].Texture;
+        nextInputIndex++;
+    }
+
+    public void SetSelectionItems(TeaItem[] items)
+    {
+        selectionItemsRaw = items;
+        for (int i = 0; i < items.Length; i++)
+        {
+            var item = items[i];
+            var selectionItem = selectionItems[i];
+            selectionItem.style.backgroundImage = item.Texture;
+        }
+    }
+
+    public void SetReferenceItems(TeaItem[] items)
+    {
+        for (int i = 0; i < items.Length; i++)
+        {
+            var item = items[i];
+            var reference = references[i];
+            reference.style.backgroundImage = item.Texture;
+        }
+    }
+
+    internal void DisableInput()
+    {
+        canInput = false;
+        inputsContainer.style.opacity = 0.5f;
+        selectionItemsContainer.style.opacity = 0.5f;
+    }
+
+    internal void EnableInput()
+    {
+        canInput = true;
+        inputsContainer.style.opacity = 1;
+        selectionItemsContainer.style.opacity = 1;
+    }
+
+    internal void ShowReferenceItems()
+    {
+        referencesContainer.style.visibility = Visibility.Visible;
+    }
+
+    internal void HideReferenceItems()
+    {
+        referencesContainer.style.visibility = Visibility.Hidden;
     }
 }
